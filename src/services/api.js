95f26c1af1a32b15ce4fa71708774c9675a5a09b
Siddhi Vandesh Patil <siddhipatil9428@gmail.com>
2025-08-01 21:38:26 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,15 @@
+// src/services/api.js
+
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-  const data = await response.json();
-  return data.results;
+  try {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching popular movies:", error);
+    return [];
+  }
 };
